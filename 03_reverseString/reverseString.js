dbe81d12b22a46f113ff28c2ae40b22608ec94d3
emilioooc <164695697+emilioooc@users.noreply.github.com>
2025-03-28 19:29:05 -0300

    Realizacion del ej 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    if (text === ''){
+        return ''
+    }else {
+        let size = text.length
+        let str = "";
+        for(let i = 0 ; i < size ; i++){
+         let wrd = text[text.length-1-i]
+            str =str.concat(wrd)
+        }
+        return str;
+    }
 };
 
 // Do not edit below this line
