482d2c2e736fc05668a67dffd86a90c8c63b0d31
emilioooc <164695697+emilioooc@users.noreply.github.com>
2025-03-28 18:43:06 -0300

    Realizacion ej 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
+/*
+const repeatString = function (text, num) {
+    if (num < 0){
+        return "ERROR"
+    }else {return text.repeat(num)}
 
 };
-
-// Do not edit below this line
 module.exports = repeatString;
+*/
+
+
+const repeatString = function (text, num){
+    let str = "";
+    if (num < 0){
+        return "ERROR"
+    }else {
+        for (let i = 0 ; i < num ; i++){
+            str = str + text
+        }
+        return str;
+    }
+}; 
+
+module.exports = repeatString;
+
+
